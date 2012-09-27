fdbc3d75148df8dfd97909f3b22083e9881b2250
Victor Wen <vic7tor@gmail.com>
2012-09-27 17:19:50 +0800

    uart driver works correct now

--- drivers/tty/serial/s3c2440.c
+++ drivers/tty/serial/s3c2440.c
@@ -25,18 +25,13 @@ enum state { STATE_OPEN, STATE_CLOSE};
 
 struct uart_info {
 	enum state state;
+	int irq_tx;
+	int irq_rx;
 };
 
 static inline void *reg_uart(struct uart_port *port, int offset)
 {
 	return (void *)(port->mapbase + offset);
-}
-
-static irqreturn_t s3c2440_uart_int_tx(int irq, void *dev_id)
-{
-	struct uart_port *port = dev_id;
-	
-	return IRQ_HANDLED;
 }
 
 static irqreturn_t s3c2440_uart_int_rx(int irq, void *dev_id)
@@ -48,6 +43,8 @@ static irqreturn_t s3c2440_uart_int_rx(int irq, void *dev_id)
 	struct uart_port *port = dev_id;
 	struct uart_info *info = port->private_data;
 	struct tty_struct *tty = port->state->port.tty;
+
+	debug(KERN_INFO "%s\n", __func__);
 
 	ufstat = ioread32(reg_uart(port, UFSTAT));
 	size = UFSTAT_RXCNT(ufstat);
@@ -85,8 +82,9 @@ static irqreturn_t s3c2440_uart_int_rx(int irq, void *dev_id)
 			goto ignore_char;
 		}
 		
-		if (info->state == STATE_OPEN)
-			uart_insert_char(port, uerstat, UERSTAT_OVERRUN, ch,
+		debug(KERN_INFO "s3c2440_uart:insert char:%c uerstat 0x%x"
+				"flag 0x%x\n", ch, uerstat, flag);
+		uart_insert_char(port, uerstat, UERSTAT_OVERRUN, ch,
 					flag);
 
 	ignore_char:
@@ -100,27 +98,53 @@ static irqreturn_t s3c2440_uart_int_rx(int irq, void *dev_id)
 
 static int s3c2440_uart_startup(struct uart_port *port)
 {
+	struct uart_info *info = port->private_data;
+	
+	debug(KERN_INFO "%s\n", __func__);
+
+	enable_irq(info->irq_rx);
 	return 0;
 }
 
 static void s3c2440_uart_shutdown(struct uart_port *port)
 {
-
+	return;
 }
 
 static void s3c2440_uart_start_tx(struct uart_port *port)
 {
+	struct circ_buf *xmit = &port->state->xmit;
+	unsigned long ufstat;
 
+	debug(KERN_INFO "s3c2440_uart:%s\n", __func__);
+
+	if (port->x_char) {
+		iowrite8(port->x_char, reg_uart(port, UTXH));
+		port->x_char = 0;
+	}
+
+	while (!uart_circ_empty(xmit)) {
+		ufstat = ioread32(reg_uart(port, UFSTAT));
+		if (ufstat & (1 << 14))
+			continue;
+
+		iowrite8(xmit->buf[xmit->tail], reg_uart(port, UTXH));
+		xmit->tail = (xmit->tail + 1) & (UART_XMIT_SIZE - 1);
+	}
 }
 
 static void s3c2440_uart_stop_tx(struct uart_port *port)
 {
-
+	struct uart_info *info = port->private_data;
+	dump_stack();
 }
 
 static void s3c2440_uart_stop_rx(struct uart_port *port)
 {
 	/* to handle sysrq when uart is not open like boot time */
+	struct uart_info *info = port->private_data;
+
+	disable_irq(info->irq_rx);
 }
 
 #ifdef CONFIG_CONSOLE_POLL
@@ -159,9 +183,9 @@ static void s3c2440_uart_set_termios(struct uart_port *port,
 	pclk = clk_get(NULL, "pclk");
 	pclk_rate = clk_get_rate(pclk);
 	baud = uart_get_baud_rate(port, new, old, 0, 8 * 115200);
-	debug("s3c2440_uart_baud=%d\n", baud);
+	debug(KERN_INFO "s3c2440_uart_baud=%d\n", baud);
 	ubrdiv = (pclk_rate / (baud * 16)) - 1;
-	debug("s3c2440 ubrdiv = %d\n", ubrdiv);
+	debug(KERN_INFO "s3c2440 ubrdiv = %d\n", ubrdiv);
 	iowrite16(ubrdiv, reg_uart(port, UBRDIV));
 
 	ulcon = 0;
@@ -201,13 +225,18 @@ static void s3c2440_uart_set_termios(struct uart_port *port,
 static void s3c2440_uart_config_port(struct uart_port *port, int flag)
 {
 	unsigned long clkcon = ioread32(REG_CLK(CLKCON));
+
+	debug(KERN_INFO "s3c2440_uart:%s\n", __func__);
+
 	clkcon |= 0x00000400 << port->line;
 	iowrite32(clkcon, REG_CLK(CLKCON));
-	iowrite32(UCON_CLOCKSEL(2) | UCON_RXTIMEOUT(1) | UCON_TXMODE(1) |
+	iowrite32(UCON_CLOCKSEL(2) | UCON_TXMODE(1) | UCON_RXTIMEOUT(1) |
 			UCON_RXMODE(1), reg_uart(port, UCON));
-	iowrite8(UFCON_TXTRIGGER(3) | UFCON_RXTRIGGER(3) | UFCON_TXFIFORST |
+	iowrite8(UFCON_TXTRIGGER(0) | UFCON_RXTRIGGER(3) | UFCON_TXFIFORST |
 			UFCON_RXFIFORST | UFCON_FIFOEN, reg_uart(port, UFCON));
 	iowrite8(0x0, reg_uart(port, UMCON));
+
+	port->type = PORT_S3C2440;
 }
 
 static void s3c2440_uart_break_ctl(struct uart_port *port, int ctl)
@@ -218,13 +247,31 @@ static void s3c2440_uart_break_ctl(struct uart_port *port, int ctl)
 	iowrite32(ucon, reg_uart(port, UCON));
 }
 
+static void s3c2440_uart_set_mctrl(struct uart_port *port, unsigned int mctrl)
+{
+
+}
+
+static unsigned int s3c2440_uart_get_mctrl(struct uart_port *prot)
+{
+	return TIOCM_CAR | TIOCM_DSR;
+}
+
+static void s3c2440_uart_enable_ms(struct uart_port *port)
+{
+
+}
+
+static struct console s3c2440_console;
+
 static struct uart_driver s3c2440_uart_driver = {
 	.owner		= THIS_MODULE,
 	.driver_name	= "s3c2440-uart",
 	.dev_name	= "ttySAC",
-	.major		= 4,
+	.major		= 204,
 	.minor		= 64,
 	.nr		= 3,
+	.cons		= &s3c2440_console,
 };
 
 static struct uart_ops s3c2440_uart_ops = {
@@ -240,25 +287,34 @@ static struct uart_ops s3c2440_uart_ops = {
 	.break_ctl	= s3c2440_uart_break_ctl,
 	.set_termios	= s3c2440_uart_set_termios,
 	.config_port	= s3c2440_uart_config_port,
+	.set_mctrl	= s3c2440_uart_set_mctrl,
+	.get_mctrl	= s3c2440_uart_get_mctrl,
+	.enable_ms	= s3c2440_uart_enable_ms,
 };
 
 static struct uart_port s3c2440_uart_port[] = {
 	[0] = {
 		.line		= 0,
+		.iotype		= UPIO_MEM,
 		.mapbase	= S3C2440_VA_UART0,
 		.fifosize	= 32,
+		.flags		= UPF_BOOT_AUTOCONF,
 		.ops		= &s3c2440_uart_ops,
 	},
 	[1] = {
 		.line		= 1,
+		.iotype		= UPIO_MEM,
 		.mapbase	= S3C2440_VA_UART1,
 		.fifosize	= 32,
+		.flags		= UPF_BOOT_AUTOCONF,
 		.ops		= &s3c2440_uart_ops,
 	},
 	[2] = {
 		.line		= 2,
+		.iotype		= UPIO_MEM,
 		.mapbase	= S3C2440_VA_UART2,
 		.fifosize	=32,
+		.flags		= UPF_BOOT_AUTOCONF,
 		.ops		= &s3c2440_uart_ops,
 	},
 };
@@ -285,12 +341,13 @@ static void s3c2440_uart_gpio_init(void)
 	iowrite32(gphcon, REG_GPIO(GPHCON));
 }
 
-static int s3c2440_uart_int_rx_init(void)
+static int s3c2440_uart_int_init(void)
 {
 	int i, j;
 	int ret;
+	j = 0;
 
-	for (i = IRQ_RXD0, j = 0; i <= IRQ_RXD2; i += 3, j++) {
+	for (i = IRQ_RXD0; i <= IRQ_RXD2; i += 3, j++) {
 		ret = request_irq(i, s3c2440_uart_int_rx, 0, "rx irq",
 				&s3c2440_uart_port[j]);
 		if (ret) {
@@ -298,6 +355,7 @@ static int s3c2440_uart_int_rx_init(void)
 					ret);
 			goto request_irq_error;
 		}
+		s3c2440_uart_info[j].irq_rx = i;
 	}
 
 	return ret;
@@ -317,7 +375,7 @@ static int s3c2440_uart_driver_init(void)
 
 	s3c2440_uart_gpio_init();
 	
-	ret = s3c2440_uart_int_rx_init();
+	ret = s3c2440_uart_int_init();
 
 	if (ret) {
 		debug(KERN_INFO "s3c2440 uart request irq error %d\n", ret);
@@ -327,13 +385,16 @@ static int s3c2440_uart_driver_init(void)
 	ret = uart_register_driver(&s3c2440_uart_driver);
 	if (ret)
 		goto error_reg_drv;
-	
+
+	debug(KERN_INFO "s3c2440_uart: register uart_driver success\n");
+
 	for (i = 0; i < ARRAY_SIZE(s3c2440_uart_port); i++) {
 		ret = uart_add_one_port(&s3c2440_uart_driver,
 				&s3c2440_uart_port[i]);
 		s3c2440_uart_port[i].private_data = &s3c2440_uart_info[i];
 
 		if (ret) {
+			printk(KERN_INFO "s3c2440_uart: add uart port fail\n");
 			for (i--; i >= 0; i--) {
 				uart_remove_one_port(&s3c2440_uart_driver,
 					&s3c2440_uart_port[i]);
